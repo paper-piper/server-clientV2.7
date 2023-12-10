0a92b861eb4b6571a5f88c3494b6b3236b474156
yoni kisho <yonatanreichert@gmail.com>
2023-12-10 15:52:38 +0200

    Added little things: - use of content variables instead of hand printed strings - Assertion - documentations

--- server.py
+++ server.py
@@ -4,7 +4,9 @@ Program name: Mini-Command-Server
 Description: Listens for commands and sends back dynamic responses.
 Date: 06-11-2023
 """
+
 import base64
+import binascii
 import glob
 import socket
 import logging
@@ -14,15 +16,26 @@ import subprocess
 import pyautogui
 import time
 
+# need to change dir re return parsed list (look nice)
+
 MAX_PACKET = 1024
 QUEUE_LEN = 1
 SERVER_ADDRESS = ('0.0.0.0', 1729)
 
 IMAGE_PATH = 'screen.jpg'
+MESSAGE_SEPARATOR = "!"
 
 # Set up logging
 logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger('server')
+
+# assert consistent
+my_script_path = "file_writer.exe"
+folder_path = 'asserts_folder'
+file_path = f"{folder_path}/test.txt"
+copied_file_path = f"{folder_path}/copied_test.txt"
+FILE_CONTENTS = "Hello!"
+executed_conformation = "Executed"
 
 
 def process_request(msg_type, msg_cont):
@@ -54,10 +67,11 @@ def process_request(msg_type, msg_cont):
                 response = "You sent an unknown command, try again or type 'exit' to exit"
         if response is None:
             response = "0"
-    except:
+    except Exception as e:
+        logger.error(f"Server Failed to create a response, ({e})")
         response = "-1"
     finally:
-        response = str(len(response)) + "!" + str(msg_type) + response
+        response = str(len(response)) + MESSAGE_SEPARATOR + str(msg_type) + response
         return response
 
 
@@ -67,14 +81,15 @@ def parse_message(sock):
     :param sock:
     :return:
     Messages protocol:
-    since message type is fixed (0-9) a separation symbol is not required
     [message content length]![message type][message content]
-    0. Exit
-    1. ...
-
     """
     len_str = ""
-    while (char := sock.recv(1).decode()) != "!":
+    while True:
+        char = sock.recv(1).decode()
+        if not char:  # Empty string indicates client disconnection
+            raise ConnectionError("Client disconnected")
+        if char == MESSAGE_SEPARATOR:
+            break
         len_str += char
     msg_len = int(len_str)
     msg_type = int(sock.recv(1).decode())
@@ -116,34 +131,6 @@ def accept_client(server_socket):
         client_socket.close()
 
 
-def dir_cmd(path) -> str:
-    return str(glob.glob(path + r"\*.*"))
-
-
-def delete_cmd(path):
-    os.remove(path)
-
-
-def copy_cmd(copy_from, copy_to):
-    shutil.copy(copy_from, copy_to)
-
-
-def execute_cmd(path):
-    subprocess.call(path)
-
-
-def take_screenshot_cmd():
-    time.sleep(5)
-    image = pyautogui.screenshot()
-    image.save(IMAGE_PATH)
-
-
-def send_photo_cmd() -> bytes:
-    with open(IMAGE_PATH, 'rb') as photo:
-        image_bytes = photo.read()
-    return image_bytes
-
-
 def main():
     """
     Initialize the server socket, accept incoming connections, and handle messages.
@@ -168,6 +155,95 @@ def main():
         logger.info("Server socket closed.")
 
 
+def dir_cmd(path) -> str:
+    """
+    return string of all files in a folder
+    :param path:
+    :return:
+    """
+    if not os.path.exists(path):
+        logger.error("User entered invalid path")
+        raise ValueError("The specified path does not exist.")
+    items = glob.glob(os.path.join(path, "*.*"))
+    formatted_items = ['- ' + os.path.basename(item) for item in items]
+    return '\n'.join(formatted_items)
+
+
+def delete_cmd(path):
+    """
+    delete file in specific path
+    :param path:
+    :return:
+    """
+    os.remove(path)
+
+
+def copy_cmd(copy_from, copy_to):
+    """
+    copy one file content to another
+    :param copy_from:
+    :param copy_to:
+    :return:
+    """
+    shutil.copy(copy_from, copy_to)
+
+
+def execute_cmd(path):
+    """
+    execute the program in the specified path
+    :param path:
+    :return:
+    """
+    subprocess.call(path)
+
+
+def take_screenshot_cmd():
+    """
+    take a screenshot and save it in IMAGE PATH
+    :return:
+    """
+    image = pyautogui.screenshot()
+    image.save(IMAGE_PATH)
+
+
+def send_photo_cmd() -> bytes:
+    """
+    get the screenshot to the client
+    :return image bytes:
+    """
+    with open(IMAGE_PATH, 'rb') as photo:
+        image_bytes = photo.read()
+    return image_bytes
+
+
 if __name__ == "__main__":
-    # Make new assertion checks
+    # checking all the server's functions
+    try:
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        with open(file_path, 'w') as file:
+            file.write(FILE_CONTENTS)
+        copy_cmd(file_path, copied_file_path)
+        assert os.path.exists(copied_file_path)
+        assert dir_cmd(folder_path).count('\n') + 1 == 2  # we add one because the first line doesn't have \n
+
+        # this .exe writes "Executed" to the file
+        execute_cmd(my_script_path)
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+            assert file_content == executed_conformation
+
+        # checking send photo, screenshot, and delete functions
+        if os.path.exists(IMAGE_PATH):
+            delete_cmd(IMAGE_PATH)
+        take_screenshot_cmd()
+        assert send_photo_cmd() is not None
+        delete_cmd(IMAGE_PATH)
+
+        # cleaning up the folder
+        shutil.rmtree(folder_path)
+    except (OSError, binascii.Error):
+        raise AssertionError("Assertion gone wrong")
     main()
+
